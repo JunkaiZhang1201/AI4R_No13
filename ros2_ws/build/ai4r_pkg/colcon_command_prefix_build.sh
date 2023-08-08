39c37bde36dd7d1272e7300ee1219982d8653434
Ze Kai Cheong <ernest.cheong.ec@gmail.com>
2023-08-08 15:08:58 +1000

    removed build install log

--- ros2_ws/build/ai4r_pkg/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/zekaicheong/asclinic-system/ros2_ws/install/ai4r_interfaces/share/ai4r_interfaces/package.sh"
--- ros2_ws/install/ai4r_interfaces/share/ai4r_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(ai4r_interfaces_IDL_FILES "msg/LeftRightFloat32.idl;msg/LeftRightInt32.idl;msg/ServoPulseWidth.idl;msg/TemplateMessage.idl")
-set(ai4r_interfaces_INTERFACE_FILES "msg/LeftRightFloat32.msg;msg/LeftRightInt32.msg;msg/ServoPulseWidth.msg;msg/TemplateMessage.msg")
