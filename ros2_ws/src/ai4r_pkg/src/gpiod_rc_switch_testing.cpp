91d358bcfbe8d493ed409be1b692c55d9f00b7f9
Ze Kai Cheong <ernest.cheong.ec@gmail.com>
2024-06-11 13:41:34 +1000

    Added simple C/C++ gpiod code for getting value of a line

---
+++ ros2_ws/src/ai4r_pkg/src/gpiod_rc_switch_testing.cpp
@@ -0,0 +1,32 @@
+#include <gpiod.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    const char * gpio_chip_name = "/dev/gpiochip0";
+    struct gpiod_chip *chip;
+    struct gpiod_line *line;
+    int val;
+
+    // GPIO line number to monitor
+    int line_number = 22;
+
+    // Open GPIO chip
+    chip = gpiod_chip_open_by_name(gpio_chip_name);
+
+    // Open GPIO lines
+    line = gpiod_chip_get_line(chip, line_number);
+
+    // Open switch line for input
+    gpiod_line_request_input(line, "example1");
+
+    // Read input gpio status
+    val = gpiod_line_get_value(line);
+    printf("GPIO line value: %d\n", val);
+
+    // Release lines and chip
+    gpiod_line_release(line);
+    gpiod_chip_close(chip);
+    return 0;
+}
