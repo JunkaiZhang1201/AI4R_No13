e7ed2da224ca07a7e5e7bf3947e9d2083d8c6455
Ze Kai Cheong <ernest.cheong.ec@gmail.com>
2024-08-07 14:31:40 +1000

    Launch traxxas_node_params.yaml from traxxas_launch.py

--- ros2_ws/src/ai4r_pkg/launch/traxxas_launch.py
+++ ros2_ws/src/ai4r_pkg/launch/traxxas_launch.py
@@ -18,12 +18,27 @@ def generate_launch_description():
         default_value='agent01'
     )
 
+    config = os.path.join(
+        get_package_share_directory('ai4r_pkg'),
+        'config',
+        'traxxas_node_params.yaml'
+        )
+    
+    # Convert the YAML config file to a Python dictionary
+    with open(config) as yaml_file:
+        # use safe_load instead load
+        config_yaml_as_dict = yaml.safe_load(yaml_file)
+
+    # Extract the element of the dictionary that needs to be passed to the node
+    parameters_dict_for_node = config_yaml_as_dict["traxxas_node"]["ros__parameters"]
+
     # Node launch details
     node=Node(
         package='ai4r_pkg',
         namespace=LaunchConfiguration('agent_ns'),
         executable='traxxas',
-        name='traxxas',
+        name='traxxas_node',
+        parameters=[parameters_dict_for_node]
     )
 
     ld.add_action(agent_ns_launch_arg)
